99dd79ef5ee684484e8213c726e3ef7533eedb6d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-18 13:38:09 +0200

    Document instructions

--- src/TIS_100/mod.rs
+++ src/TIS_100/mod.rs
@@ -35,15 +35,21 @@ impl Eq for Node {}
 
 /// `Instruction`s are executed by a `Node`.
 pub enum Instruction {
+    /// Does nothing
     NOP,
+    /// Moves value from `Source` to `Destination`
     MOV(Source, Destination),
+    /// Swaps the value of the accumulator (acc) and the backup (bac) register
     SWP,
+    /// Saves the value of the accumulator (acc) to the backup register
     SAV,
+    /// Add value from `Source` to accumulator (acc), storing result in acc
     ADD(Source),
+    /// Subtracts value from `Source` from accumulator (acc), storing result in acc
     SUB(Source),
 }
 
-/// Source are either ports, registers or literals
+/// `Source` are either ports, registers or literals
 pub enum Source {
     NIL,
     ACC,
@@ -51,7 +57,7 @@ pub enum Source {
 }
 
 
-/// Destination are either ports or registers
+/// `Destination` are either ports or registers
 pub enum Destination {
     NIL,
     ACC
