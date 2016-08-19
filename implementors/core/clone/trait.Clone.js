20d3505ce1b19d32cba05ab254541947964a3899
Daan van Berkel <daan@fifth-postulate.nl>
2016-08-19 19:17:08 +0000

    rebuild pages at 877f768

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tis_100_superoptimizer"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='tis_100_superoptimizer/TIS_100/struct.Program.html' title='tis_100_superoptimizer::TIS_100::Program'>Program</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='tis_100_superoptimizer/TIS_100/enum.Instruction.html' title='tis_100_superoptimizer::TIS_100::Instruction'>Instruction</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='tis_100_superoptimizer/TIS_100/enum.Source.html' title='tis_100_superoptimizer::TIS_100::Source'>Source</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='tis_100_superoptimizer/TIS_100/enum.Register.html' title='tis_100_superoptimizer::TIS_100::Register'>Register</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='tis_100_superoptimizer/TIS_100/enum.Destination.html' title='tis_100_superoptimizer::TIS_100::Destination'>Destination</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
