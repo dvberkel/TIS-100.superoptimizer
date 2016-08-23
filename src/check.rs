62e44de8d1468712171f7fe3476047809c6d4585
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-23 14:26:35 +0200

    Create a check method

---
+++ src/check.rs
@@ -0,0 +1,49 @@
+//! The check module verifies if a certain `Program` when run on a specific
+//! `Node` produces the expected result. I.e. Reads the input on `Source::Port`
+//! and writes the correct sequence the `Destination::Port`.
+
+use super::TIS_100::{Node, Program};
+
+/// Checks if `Node` when run with `Program` writes `expected_result` to `Destination::Port`
+pub fn check(node: Node, program: Program, expected_result: Vec<i32>) -> bool {
+    let local_node: Node = node.load(program);
+
+    match local_node.run() {
+        Ok(result_node) => {
+            same(result_node.down.output, expected_result)
+        },
+        Err(_) => false,
+    }
+}
+
+fn same<T: Eq>(left: Vec<T>, right: Vec<T>) -> bool {
+    if left.len() == right.len() {
+        for index in 0..(left.len()) {
+            if left[index] != right[index] {
+                return false;
+            }
+        }
+        true
+    } else {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::TIS_100::{Node, Program, Instruction, Source, Destination, Register};
+    use super::super::TIS_100::Ports::Port;
+    use super::*;
+
+    #[test]
+    fn should_correctly_check_program() {
+        let program: Program = Program(vec![
+            Instruction::MOV(Source::Port, Destination::Register(Register::ACC)),
+            Instruction::ADD(Source::Port),
+            Instruction::MOV(Source::Register(Register::ACC), Destination::Port),
+        ]);
+        let node: Node = Node::new().set_up(Port::new(vec![0, 1, 2, 3]));
+
+        assert!(check(node, program, vec![1, 5]))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,4 @@
 
 #[allow(non_snake_case)]
 pub mod TIS_100;
+pub mod check;
