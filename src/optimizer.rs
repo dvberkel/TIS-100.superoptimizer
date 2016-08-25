20269a59721b698b28c6fb40affcefc68b4bd63f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-08-25 08:20:19 +0200

    Remove unnecessary iterators
    We changed strategy over how to iterate over programs

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -123,83 +123,10 @@ fn instruction_map(index: u32) -> Instruction {
     }
 }
 
-/// Iterator over Destination
-pub struct DestinationIterator {
-    current: i32,
-    register_iterator: RegisterIterator,
-}
-
-impl DestinationIterator {
-    /// create a DestinationIterator
-    fn new() -> DestinationIterator {
-        DestinationIterator {
-            current: 0,
-            register_iterator: RegisterIterator::new(),
-        }
-    }
-}
-
-impl Iterator for DestinationIterator {
-    type Item = Destination;
-
-    fn next(&mut self) -> Option<Destination> {
-        let next_destination = match self.current {
-            0 => Some(Destination::Port),
-            _ => {
-                self.register_iterator.next().map(|register| Destination::Register(register))
-            }
-        };
-        self.current += 1;
-        next_destination
-    }
-}
-
-/// Iterator over Registers
-pub struct RegisterIterator {
-    current: u32,
-}
-
-impl RegisterIterator {
-    fn new() -> RegisterIterator {
-        RegisterIterator { current: 0 }
-    }
-}
-
-impl Iterator for RegisterIterator {
-    type Item = Register;
-
-    fn next(&mut self) -> Option<Register> {
-        let result = match self.current {
-            0 => Some(Register::NIL),
-            1 => Some(Register::ACC),
-            _ => None
-        };
-        self.current += 1;
-        result
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::super::TIS_100::{Program,Instruction,Source,Destination,Register};
     use super::*;
-
-    #[test]
-    fn should_iterator_over_destinations() {
-        let destinations: Vec<Destination> = DestinationIterator::new().collect();
-
-        assert_eq!(Destination::Port, destinations[0]);
-        assert_eq!(Destination::Register(Register::NIL), destinations[1]);
-        assert_eq!(Destination::Register(Register::ACC), destinations[2]);
-    }
-
-    #[test]
-    fn should_iterator_over_registers() {
-        let registers: Vec<Register> = RegisterIterator::new().collect();
-
-        assert_eq!(Register::NIL, registers[0]);
-        assert_eq!(Register::ACC, registers[1]);
-    }
 
     #[test]
     fn should_iterate_over_programs() {
